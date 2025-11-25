f1563a6a6b2efa7f0cde9ec85267aa32c7ffd422
Mayur Kankale <MayurK@sycasol>
2025-11-25 15:13:01 +0530

    update movie detail file

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -2,31 +2,36 @@ import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 
 function MovieDetail() {
-const {id} = useParams()
-const [movie, setMovie] = useState(null)
+  const { id } = useParams()
+  const [movie, setMovie] = useState(null)
 
- useEffect(() => {
+  useEffect(() => {
+    async function getMovie() {
+      const apiKey = import.meta.env.VITE_OMDB_API_KEY;
 
-async function getMovie() {
+      const response = await fetch(
+        `https://www.omdbapi.com/?apikey=${apiKey}&i=${id}`
+      )
 
-    const response = await fetch(`http://www.omdbapi.com/?=tt3896198&apikey=245b275e&i=${id}`)
-    const data = await response.json();
-    console.log(data)
-    setMovie(data)
-}
-getMovie();
-} , [id])
-if(!movie) return <div>Loading...</div>
+      const data = await response.json()
+      console.log(data)
+      setMovie(data)
+    }
 
-    return (
-        <div className="movie-detail">
-		<h2> {movie.Title}</h2>
-		<img alt= {movie.Title} src={movie.Poster}/>
-		<p><strong>Genre:</strong> {movie.Genre}</p>
-		<p><strong>Released:</strong> {movie.Released}</p>
-		<p><strong>Plot:</strong> {movie.Plot}</p>
-	</div>
-    )
+    getMovie()
+  }, [id])
+
+  if (!movie) return <div>Loading...</div>
+
+  return (
+    <div className="movie-detail">
+      <h2>{movie.Title}</h2>
+      <img alt={movie.Title} src={movie.Poster} />
+      <p><strong>Genre:</strong> {movie.Genre}</p>
+      <p><strong>Released:</strong> {movie.Released}</p>
+      <p><strong>Plot:</strong> {movie.Plot}</p>
+    </div>
+  )
 }
 
 export default MovieDetail
