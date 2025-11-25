0a56ac63d6bf3487c8238c606339c1cce651ad70
Mayur Kankale <MayurK@sycasol>
2025-11-25 15:09:01 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,53 @@
-
 import { useState, useEffect, useRef } from "react"
 import MovieList from "../components/MovieList"
+
 function Home() {
 
-const [movies, setMovies] = useState([])
-const [loading, setLoading] = useState(false)
-const inputRef = useRef()
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const inputRef = useRef()
 
-const fetchMovies = async (query) => {
+  const fetchMovies = async (query) => {
     setLoading(true)
-    const response = await fetch(`http://www.omdbapi.com/?=tt3896198&apikey=245b275e&s=${query}`)
+
+    const apiKey = import.meta.env.VITE_OMDB_API_KEY
+
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${apiKey}&s=${query}`
+    )
+
     const data = await response.json()
     console.log(data)
     setMovies(data.Search || [])
     setLoading(false)
-}
+  }
 
-useEffect(() => {
-    fetchMovies('Avengers')
-}, [])
+  useEffect(() => {
+    fetchMovies("Avengers")
+  }, [])
 
-const handlesearch = (e) => {
+  const handlesearch = (e) => {
     e.preventDefault()
-    const query = inputRef.current.value.trim();
-    if(query) {
-        fetchMovies(query)
+    const query = inputRef.current.value.trim()
+    if (query) {
+      fetchMovies(query)
     }
-}
+  }
 
   return (
-<div className="home">
-    <form onSubmit={handlesearch}>
-        <input ref={inputRef} className="searchInput" placeholder="Search for a movie..."/>
+    <div className="home">
+      <form onSubmit={handlesearch}>
+        <input
+          ref={inputRef}
+          className="searchInput"
+          placeholder="Search for a movie..."
+        />
         <button type="submit">Search 🔎</button>
-    </form>
-    {loading ? <p>Loading...</p> :  <MovieList movies={movies} />}
-    
-</div>	
+      </form>
+
+      {loading ? <p>Loading...</p> : <MovieList movies={movies} />}
+    </div>
   )
-  
 }
 
 export default Home
